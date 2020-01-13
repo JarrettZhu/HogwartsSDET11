5e9c94c0a623b344e85d2eb136a787d1e41743f3
猪俊华 <353782934@qq.com>
2020-01-13 18:51:37 +0800

    修改div函数测试用例test_div.py为参数化传递测试数据

--- unit/test_div.py
+++ unit/test_div.py
@@ -4,40 +4,46 @@ from unit.div import div
 
 
 class Testjob1:
-    # 缺点，assert错误马上退出，不会执行后续的断言
-    @pytest.mark.happy
-    def test_div_int(self):
-        assert div(10, 5) == 2
-        assert div(10, 2) == 5
-        assert div(1000000, 1) == 1000000
 
     # 参数化，遇到assert错误不会直接退出，更灵活
     @pytest.mark.happy
-    @pytest.mark.parametrize("number1, number2, expection", {
+    @pytest.mark.parametrize("number_1, number_2, expection", {
         (10, 2, 5),
         (10, 5, 3),
         (1000000, 1, 1000000),
         (100, 200, 0.5)
     })
-    def test_div_int_param(self, number1, number2, expection):
-        assert div(number1, number2) == expection
+    def test_div_int_param(self, number_1, number_2, expection):
+        assert div(number_1, number_2) == expection
 
     @pytest.mark.happy
-    def test_div_float(self):
-        assert div(10, 3) == 3.3333333
-        assert div(10.2, 0.2) == 51
+    @pytest.mark.parametrize("float_number_1, float_number_2, expection", {
+        (10, 3, 3.3333333),
+        (10.2, 0.2, 51)
+    })
+    def test_div_float(self, float_number_1, float_number_2, expection):
+        assert div(float_number_1, float_number_2) == expection
 
     @pytest.mark.exception
-    def test_div_exception(self):
-        assert div(10, 'a')
-        assert div('abc', 10)
+    @pytest.mark.parametrize("value_1, value_2", {
+        (10, 'a'),
+        ('abc', 10)
+    })
+    def test_div_exception(self, value_1, value_2):
+        assert div(value_1, value_2)
 
     @pytest.mark.happy
-    def test_div_zero(self):
-        assert div(10, 0) == None
+    @pytest.mark.parametrize("value_1, zero, expection", {
+        (10, 0, None)
+    })
+    def test_div_zero(self, value_1, zero, expection):
+        assert div(value_1, zero) == expection
 
     # 预期出现除0错误异常
     @pytest.mark.exception
-    def test_zero_division(self):
+    @pytest.mark.parametrize("value_1, value_2", {
+        (1, 0)
+    })
+    def test_zero_division(self, value_1, value_2):
         with pytest.raises(ZeroDivisionError):
-            1 / 0
+            value_1 / value_2
